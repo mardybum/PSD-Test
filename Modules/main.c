e6242b6e98f94d309a10b2feb971d2b5a41e8d37
Yan <y.rudall@gmail.com>
2015-05-27 07:53:15 +0200

    NEW BRANCH COMMMIT

--- Modules/main.c
+++ Modules/main.c
@@ -20,11 +20,7 @@ int main(void) {
 	int variable = 1;
 	int b = "bcadsfgsdg"; //Error
 
-	int variable = 1;
-	int b = "bcadsfgsdg"; //Error
-	
-	int variable = 1;
-	int b = "bcadsfgsdg"; //Error
+	String c = 35;
 	
 	doNothing(b);
 
